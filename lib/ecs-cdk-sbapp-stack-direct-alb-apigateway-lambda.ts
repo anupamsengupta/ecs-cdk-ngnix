0dab0ff5ea805094810d2247c96eb57711ac6c43
Anupam Sengupta <anupamsen@yahoo.com>
2024-10-06 19:09:27 +0530

    added backend service

--- lib/ecs-cdk-sbapp-stack-direct-alb-apigateway-lambda.ts
+++ lib/ecs-cdk-sbapp-stack-direct-alb-apigateway-lambda.ts
@@ -9,10 +9,12 @@ import * as path from "path";
 import { QSNetworkStack } from "./qs-network-stack";
 import * as ecr from 'aws-cdk-lib/aws-ecr';  // Import ECR repository
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as servicediscovery from 'aws-cdk-lib/aws-servicediscovery'; // Import Cloud Map
 
 export class EcsCdkSBAppDirectStackSimple extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const lambdaTimeoutSeconds = 15; //TO-DO need to go to props
 
     // Create a VPC and overall network
     const networkClusterStack = new QSNetworkStack(
@@ -26,8 +28,18 @@ export class EcsCdkSBAppDirectStackSimple extends cdk.Stack {
         azs: ["us-east-1a", "us-east-1b"],
       }
     );
-
     const vpc: ec2.IVpc = networkClusterStack.network.vpc;
+
+    // Create a Cloud Map namespace for service discovery
+    const springbootAppNamespace = new servicediscovery.PrivateDnsNamespace(
+      this,
+      "springBootAppSharedPrivateNamespace",
+      {
+        name: "springBootAppSharedPrivateNamespace",
+        vpc: vpc,
+      }
+    );
+
     // Create an ECS cluster
     const cluster = new ecs.Cluster(this, "Test-ECS-Cluster", {
       vpc: vpc,
@@ -58,57 +70,160 @@ export class EcsCdkSBAppDirectStackSimple extends cdk.Stack {
       assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
     });
 
+
+    // Create a Fargate task definition for Backend
+    const backendTaskDefinition = new ecs.FargateTaskDefinition(
+      this,
+      "backendTaskDef",
+      {
+        memoryLimitMiB: 512,
+        cpu: 256,
+        executionRole: taskExecutionRole, // Set execution role for ECR pull
+      }
+    );
+
+    const backendContainer = backendTaskDefinition.addContainer(
+      "backendContainer",
+      {
+        image: ecs.ContainerImage.fromEcrRepository(privateEcrRepo, "latest"), // Specify tag if needed
+        logging: ecs.LogDrivers.awsLogs({ streamPrefix: "BackendContainer" }),
+        environment: {
+          DB_URL: "db@serviceIP:onPort",
+          secretsmanagerkey: "secretsmanagerkey_value",
+          APP_CONTEXT_PATH: "/backend",
+        },
+        portMappings: [{ containerPort: 80 }],
+      }
+    );
+
+    // Create a Fargate service for Backend
+    const backendService = new ecs.FargateService(this, "BackendService", {
+      cluster,
+      taskDefinition: backendTaskDefinition,
+      desiredCount: 1,
+      securityGroups: [ecsSecurityGroup],
+      cloudMapOptions: {
+        name: "backendapi",
+        cloudMapNamespace: springbootAppNamespace,
+      },
+    });
+
+
     // Attach AmazonECSTaskExecutionRolePolicy for ECR image pull permissions
     taskExecutionRole.addManagedPolicy(
       iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonECS_FullAccess")
     );
 
-    // Create a Fargate task definition
+    // Create a Fargate task definition - sbapp-1
     const sbapp1TaskDefinition = new ecs.FargateTaskDefinition(this, "sbapp1TaskDef", {
       memoryLimitMiB: 512,
       cpu: 256,
       executionRole: taskExecutionRole, // Set execution role for ECR pull
     });
 
+    // Create a Container to Fargate task definition - sbapp-1
     const sbapp1Container = sbapp1TaskDefinition.addContainer("sbapp1", 
       {
         image: ecs.ContainerImage.fromEcrRepository(privateEcrRepo, "latest"), // Specify tag if needed
-        logging: ecs.LogDrivers.awsLogs({ streamPrefix: "sbappContainer" }),
+        logging: ecs.LogDrivers.awsLogs({ streamPrefix: "sbapp1Container" }),
         environment: {
           DB_URL: "db@serviceIP:onPort",
           secretsmanagerkey: "secretsmanagerkey_value",
           APP_CONTEXT_PATH: "/sbappx1",
+          EXTERNAL_GET_URL1: `http://backendapi.springBootAppSharedPrivateNamespace/backend/api/external-api`,
+          EXTERNAL_GET_URL2: `http://backendapi.springBootAppSharedPrivateNamespace/backend/api/greet`,
         },
         portMappings: [{ containerPort: 80 }],
       }
     );
 
-    // Create a Fargate service
+    // Create a Fargate service - sbapp-1
     const sbapp1Service = new ecs.FargateService(this, "sbapp1Service", {
       cluster,
       taskDefinition: sbapp1TaskDefinition,
       desiredCount: 1,
       securityGroups: [ecsSecurityGroup],
+      cloudMapOptions: {
+        name: "sbapp1",
+        cloudMapNamespace: springbootAppNamespace,
+      },
+    });
+
+    // Create a Fargate task definition - sbapp-2
+    const sbapp2TaskDefinition = new ecs.FargateTaskDefinition(this, "sbapp2TaskDef", {
+      memoryLimitMiB: 512,
+      cpu: 256,
+      executionRole: taskExecutionRole, // Set execution role for ECR pull
+    });
+
+    // Create a Container to Fargate task definition - sbapp-1
+    const sbapp2Container = sbapp2TaskDefinition.addContainer("sbapp2", 
+      {
+        image: ecs.ContainerImage.fromEcrRepository(privateEcrRepo, "latest"), // Specify tag if needed
+        logging: ecs.LogDrivers.awsLogs({ streamPrefix: "sbapp2Container" }),
+        environment: {
+          DB_URL: "db@serviceIP:onPort",
+          secretsmanagerkey: "secretsmanagerkey_value",
+          APP_CONTEXT_PATH: "/sbappx2",
+          EXTERNAL_GET_URL1: `http://backendapi.springBootAppSharedPrivateNamespace/backend/api/external-api`,
+          EXTERNAL_GET_URL2: `http://backendapi.springBootAppSharedPrivateNamespace/backend/api/greet`,
+        },
+        portMappings: [{ containerPort: 80 }],
+      }
+    );
+
+    // Create a Fargate service - sbapp-2
+    const sbapp2Service = new ecs.FargateService(this, "sbapp2Service", {
+      cluster,
+      taskDefinition: sbapp2TaskDefinition,
+      desiredCount: 1,
+      securityGroups: [ecsSecurityGroup],
+      cloudMapOptions: {
+        name: "sbapp2",
+        cloudMapNamespace: springbootAppNamespace,
+      },
     });
 
     // Create an Application Load Balancer (ALB)
-    const lb = new elbv2.ApplicationLoadBalancer(this, "LB", {
+    const sbappAlb = new elbv2.ApplicationLoadBalancer(this, "LB", {
       vpc,
       internetFacing: false,
     });
 
-    const listener = lb.addListener("sbapp1Listener", {
+    const listener = sbappAlb.addListener("sbappListener", {
       port: 80,
       open: true,
     });
 
     // Attach the ECS service to the ALB
-    listener.addTargets("ECS", {
+    listener.addTargets("defaultECS", {
       port: 80,
       targets: [sbapp1Service],
       healthCheck: {
         interval: cdk.Duration.seconds(10),
         path: "/sbappx1/actuator/health",
+        timeout: cdk.Duration.seconds(5),
+      },
+    });
+    listener.addTargets("sbapp1ECS", {
+      port: 80,
+      targets: [sbapp1Service],
+      conditions: [elbv2.ListenerCondition.pathPatterns(['/sbappx1/*'])],
+      priority: 1,
+      healthCheck: {
+        interval: cdk.Duration.seconds(10),
+        path: "/sbappx1/actuator/health",
+        timeout: cdk.Duration.seconds(5),
+      },
+    });
+    listener.addTargets("sbapp2ECS", {
+      port: 80,
+      targets: [sbapp2Service],
+      conditions: [elbv2.ListenerCondition.pathPatterns(['/sbappx2/*'])],
+      priority: 2,
+      healthCheck: {
+        interval: cdk.Duration.seconds(10),
+        path: "/sbappx2/actuator/health",
         timeout: cdk.Duration.seconds(5),
       },
     });
@@ -119,9 +234,11 @@ export class EcsCdkSBAppDirectStackSimple extends cdk.Stack {
       handler: "index.handler",
       code: lambda.Code.fromAsset(path.join(__dirname, "lambda")),
       environment: {
-        ALB_DNS_NAME: lb.loadBalancerDnsName,
+        ALB_DNS_NAME: sbappAlb.loadBalancerDnsName,
       },
+      timeout: cdk.Duration.seconds(lambdaTimeoutSeconds),
       vpc: vpc,
+      securityGroups: [ecsSecurityGroup],
     });
 
 
@@ -132,19 +249,31 @@ export class EcsCdkSBAppDirectStackSimple extends cdk.Stack {
     });
 
     // Create a GET method
-    const getIntegration = new apigateway.LambdaIntegration(lambdaFunction);
-    api.root.addMethod("GET", getIntegration);
+    const lambdaDelegationIntegration = new apigateway.LambdaIntegration(lambdaFunction);
+    api.root.addMethod("GET", lambdaDelegationIntegration);
 
 
     // Create REST resources
-    const mainResource = api.root.addResource('sbappx1');
-    const acctuatorResource = mainResource.addResource('actuator');
-    const healthResource = acctuatorResource.addResource("health");
+    const mainResource1 = api.root.addResource('sbappx1');
+    mainResource1
+      .addResource("{proxy+}")
+      .addMethod("ANY", lambdaDelegationIntegration, {
+        requestParameters: {
+          "method.request.path.proxy": true, // Enable path proxying
+        },
+      });
 
-    healthResource.addMethod("GET", getIntegration);
+    const mainResource2 = api.root.addResource('sbappx2');
+    mainResource2
+    .addResource("{proxy+}")
+    .addMethod("ANY", lambdaDelegationIntegration, {
+      requestParameters: {
+        "method.request.path.proxy": true, // Enable path proxying
+      },
+    });
 
     new cdk.CfnOutput(this, "LoadBalancerDNS", {
-      value: lb.loadBalancerDnsName,
+      value: sbappAlb.loadBalancerDnsName,
     });
   }
 }
