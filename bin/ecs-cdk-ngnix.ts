880cc80198fb6dec6ef389b2a2522ce7e3e48509
Anupam Sengupta <anupamsen@yahoo.com>
2024-10-12 15:34:42 +0530

    added api-nlb-ecs

--- bin/ecs-cdk-ngnix.ts
+++ bin/ecs-cdk-ngnix.ts
@@ -3,10 +3,11 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 //import { EcsCdkSpringBootAppStackVPCLinkAndNLB } from '../lib/ecs-cdk-sbapp-stack-vpc-link-nlb';
 //import { EcsCdkSpringBootAppStackLambdaAndALB } from '../lib/ecs-cdk-sbapp-stack-simple-alb-apigateway-lambda';
-import { EcsCdkSBAppDirectStackSimple } from '../lib/ecs-cdk-sbapp-stack-direct-alb-apigateway-lambda-2';
+//import { EcsCdkSBAppDirectStackSimple } from '../lib/ecs-cdk-sbapp-stack-direct-alb-apigateway-lambda-2';
+import {EcsCdkSimpleApiNlbEcsDemoStack} from '../lib/simple-api-nlb-ecs';
 
 const app = new cdk.App();
-new EcsCdkSBAppDirectStackSimple(app, 'ecsCdkSBAppDirectStackSimple1', {
+new EcsCdkSimpleApiNlbEcsDemoStack(app, 'ecsCdkSimpleApiNlbEcsDemoStack', {
   env : {region : 'us-east-1'}
 });
 app.synth();
