2f4def381cc71aeec97505b85fda8304019c54a9
Anupam Sengupta <anupamsen@yahoo.com>
2024-10-12 18:56:59 +0530

    added modular change-ph1

--- bin/ecs-cdk-ngnix.ts
+++ bin/ecs-cdk-ngnix.ts
@@ -5,10 +5,11 @@ import * as cdk from 'aws-cdk-lib';
 //import { EcsCdkSpringBootAppStackLambdaAndALB } from '../lib/ecs-cdk-sbapp-stack-simple-alb-apigateway-lambda';
 //import { EcsCdkSBAppDirectStackSimple } from '../lib/ecs-cdk-sbapp-stack-direct-alb-apigateway-lambda-2';
 //import {EcsCdkSimpleApiNlbEcsDemoStack} from '../lib/simple-api-nlb-ecs';
-import {EcsCdkSimpleApiNlbAlbEcsDemoStack} from '../lib/simple-api-nlb-alb-ecs';
+//import {EcsCdkSimpleApiNlbAlbEcsDemoStack} from '../lib/simple-api-nlb-alb-ecs';
+import {EcsCdkSimpleApiNlbAlbEcsModularDemoStack} from '../lib/simple-api-nlb-alb-ecs-modular';
 
 const app = new cdk.App();
-new EcsCdkSimpleApiNlbAlbEcsDemoStack(app, 'ecsCdkSimpleApiNlbAlbEcsDemoStack', {
+new EcsCdkSimpleApiNlbAlbEcsModularDemoStack(app, 'ecsCdkSimpleApiNlbAlbEcsModularDemoStack', {
   env : {region : 'us-east-1'}
 });
 app.synth();
--- lib/simple-api-nlb-alb-ecs-modular.ts
+++ lib/simple-api-nlb-alb-ecs-modular.ts
@@ -9,13 +9,15 @@ import * as servicediscovery from 'aws-cdk-lib/aws-servicediscovery'; // Import 
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as ecr from 'aws-cdk-lib/aws-ecr';  // Import ECR repository
 import { AlbListenerTarget } from "aws-cdk-lib/aws-elasticloadbalancingv2-targets";
+import { QSClusterMain } from "./qs-ecs-cluster";
+import { IQSTask, QSTaskMain } from "./qs-ecs-task";
 
-export class EcsCdkSimpleApiNlbAlbEcsDemoStack extends cdk.Stack {
+export class EcsCdkSimpleApiNlbAlbEcsModularDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // Create a VPC and overall network
-    const networkStack = new QSNetworkStack(scope, "ecsNetworkStackName1", {
+    const clusterNetworkStack = new QSNetworkStack(scope, "ecsNetworkStackName1", {
       env: {
         region: "us-east-1",
       },
@@ -23,35 +25,24 @@ export class EcsCdkSimpleApiNlbAlbEcsDemoStack extends cdk.Stack {
       azs: ["us-east-1a", "us-east-1b"],
     });
 
-    const vpc: ec2.IVpc = networkStack.network.vpc;
+    const vpc: ec2.IVpc = clusterNetworkStack.network.vpc;
+    let namespace = "springBootAppSharedPrivateNamespace";
+    //Create the cluster, roles and namespaces
+    const clusterConstruct = new QSClusterMain(this, "sbAppCluster", {
+      network: clusterNetworkStack.network,
+      stackName: "sbApp",
+      serviceDiscoveryNamespace: namespace,
+    });
+
     // Create an ECS cluster
-    const cluster = new ecs.Cluster(this, "Test-ECS-Cluster", {
-      vpc: vpc,
-    });
-
+    const cluster = clusterConstruct.cluster;
     // Create a Cloud Map namespace for service discovery
-    const cloudmapNamespace = new servicediscovery.PrivateDnsNamespace(
-      this,
-      "Namespace",
-      {
-        name: "ecsnamespace",
-        vpc: vpc,
-      }
-    );
-
-    // Create a security group for the ECS service
-    const ecsSecurityGroup = new ec2.SecurityGroup(this, "EcsSecurityGroup", {
-      vpc,
-      description: "Allow traffic from NLB",
-      allowAllOutbound: true,
-    });
-
-    // Allow inbound traffic from the NLB on port 80
-    ecsSecurityGroup.addIngressRule(
-      ec2.Peer.ipv4(vpc.vpcCidrBlock),
-      ec2.Port.tcp(80),
-      "Allow traffic from NLB"
-    );
+    const springbootAppNamespace = clusterConstruct.appNamespace;
+    // Task Execution Role with AmazonECSTaskExecutionRolePolicy attached
+    const taskExecutionRole = clusterConstruct.taskExecutionRole;
+    //security group
+    const ecsSecurityGroup =
+      clusterNetworkStack.network.preconfiguredVpcCidrAccessHttpSecurityGroup;
 
     const privateEcrRepo = ecr.Repository.fromRepositoryName(
       this,
@@ -59,142 +50,64 @@ export class EcsCdkSimpleApiNlbAlbEcsDemoStack extends cdk.Stack {
       "quickysoft/sample-spring-boot-app"
     );
 
-    // Task Execution Role with AmazonECSTaskExecutionRolePolicy attached
-    const taskExecutionRole = new iam.Role(this, "TaskExecutionRole", {
-      assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
-    });
-
-    // Attach AmazonECSTaskExecutionRolePolicy for ECR image pull permissions
-    taskExecutionRole.addManagedPolicy(
-      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonECS_FullAccess")
-    );
-
-    // Create a Fargate task definition for Backend
-    const backendTaskDefinition = new ecs.FargateTaskDefinition(
-      this,
-      "backendTaskDef",
-      {
-        memoryLimitMiB: 512,
-        cpu: 256,
-        executionRole: taskExecutionRole, // Set execution role for ECR pull
-      }
-    );
-
-    const backendContainer = backendTaskDefinition.addContainer("backend", {
-      image: ecs.ContainerImage.fromEcrRepository(privateEcrRepo, "latest"), // Specify tag if needed
-      //image: externalEcrImage,
-      logging: ecs.LogDrivers.awsLogs({ streamPrefix: "backend" }),
-      environment: {
-        APP_CONTEXT_PATH: "/backend",
-        DB_URL: "db@serviceIP:onPort",
-        secretsmanagerkey: "secretsmanagerkey_value",
-      },
-    });
-
-    backendContainer.addPortMappings({
-      containerPort: 80,
-    });
-
-    // Create a Fargate service for Backend
-    const backendService = new ecs.FargateService(this, "BackendService", {
-      cluster,
-      taskDefinition: backendTaskDefinition,
+    const backendTask: IQSTask = new QSTaskMain(this, "backend", {
+      stackName: this.stackName,
+      taskName: "backend",
+      cluster: clusterConstruct.cluster,
+      memoryLimitMiB: 512,
+      cpu: 256,
+      repo: privateEcrRepo,
+      repoTag: "latest",
+      mappedPort: 80,
       desiredCount: 1,
-      securityGroups: [ecsSecurityGroup],
-      cloudMapOptions: {
-        name: "backendapi",
-        cloudMapNamespace: cloudmapNamespace,
-      },
+      securityGroup:
+        clusterNetworkStack.network.preconfiguredVpcCidrAccessHttpSecurityGroup,
+      executionRole: clusterConstruct.taskExecutionRole, // Set execution role for ECR pull
+      serviceDiscoveryNamespace: springbootAppNamespace,
+      DB_URL: "db@serviceIP:onPort",
+      secretsmanagerkey: "secretsmanagerkey_value",
+      EXTERNAL_GET_URL1: `http://localhost/backend/api/external-api`,
+      EXTERNAL_GET_URL2: `http://localhost/backend/api/greet`,
     });
 
-    // Create a Fargate task definition
-    const frontend1TaskDefinition = new ecs.FargateTaskDefinition(
-      this,
-      "frontend1TaskDef",
-      {
-        memoryLimitMiB: 512,
-        cpu: 256,
-        executionRole: taskExecutionRole, // Set execution role for ECR pull
-      }
-    );
-
-    const frontend1AppContainer = frontend1TaskDefinition.addContainer(
-      "frontend1Service",
-      {
-        image: ecs.ContainerImage.fromEcrRepository(privateEcrRepo, "latest"), // Specify tag if needed
-        //image: externalEcrImage,
-        logging: ecs.LogDrivers.awsLogs({ streamPrefix: "frontend1" }),
-        environment: {
-          APP_CONTEXT_PATH: "/frontend1",
-          DB_URL: "db@serviceIP:onPort",
-          secretsmanagerkey: "secretsmanagerkey_value",
-          EXTERNAL_GET_URL1:
-            "http://backendapi.ecsnamespace/backend/api/greet",
-          EXTERNAL_GET_URL2:
-            "http://backendapi.ecsnamespace/backend/api/external-api",
-        },
-      }
-    );
-
-    frontend1AppContainer.addPortMappings({
-      containerPort: 80,
-    });
-
-    // Create a Fargate service
-    const frontend1AppService = new ecs.FargateService(this, "frontend1Service", {
-      cluster,
-      taskDefinition: frontend1TaskDefinition,
+    const frontendTask1: IQSTask = new QSTaskMain(this, "frontend1", {
+      stackName: this.stackName,
+      taskName: "frontend1",
+      cluster: clusterConstruct.cluster,
+      memoryLimitMiB: 512,
+      cpu: 256,
+      repo: privateEcrRepo,
+      repoTag: "latest",
+      mappedPort: 80,
       desiredCount: 1,
-      securityGroups: [ecsSecurityGroup],
-      cloudMapOptions: {
-        name: "frontend1api",
-        cloudMapNamespace: cloudmapNamespace,
-      },
+      securityGroup:
+        clusterNetworkStack.network.preconfiguredVpcCidrAccessHttpSecurityGroup,
+      executionRole: clusterConstruct.taskExecutionRole, // Set execution role for ECR pull
+      serviceDiscoveryNamespace: springbootAppNamespace,
+      DB_URL: "db@serviceIP:onPort",
+      secretsmanagerkey: "secretsmanagerkey_value",
+      EXTERNAL_GET_URL1: `http://backendapi.sbApp-springBootAppSharedPrivateNamespace/backend/api/external-api`,
+      EXTERNAL_GET_URL2: `http://backendapi.sbApp-springBootAppSharedPrivateNamespace/backend/api/greet`,
     });
 
-    // Create a Fargate task definition
-    const frontend2TaskDefinition = new ecs.FargateTaskDefinition(
-      this,
-      "frontend2TaskDef",
-      {
-        memoryLimitMiB: 512,
-        cpu: 256,
-        executionRole: taskExecutionRole, // Set execution role for ECR pull
-      }
-    );
-
-    const frontend2AppContainer = frontend2TaskDefinition.addContainer(
-      "frontend2Service",
-      {
-        image: ecs.ContainerImage.fromEcrRepository(privateEcrRepo, "latest"), // Specify tag if needed
-        //image: externalEcrImage,
-        logging: ecs.LogDrivers.awsLogs({ streamPrefix: "frontend2" }),
-        environment: {
-          APP_CONTEXT_PATH: "/frontend2",
-          DB_URL: "db@serviceIP:onPort",
-          secretsmanagerkey: "secretsmanagerkey_value",
-          EXTERNAL_GET_URL1:
-            "http://backendapi.ecsnamespace/backend/api/greet",
-          EXTERNAL_GET_URL2:
-            "http://backendapi.ecsnamespace/backend/api/external-api",
-        },
-      }
-    );
-
-    frontend2AppContainer.addPortMappings({
-      containerPort: 80,
-    });
-
-    // Create a Fargate service
-    const frontend2AppService = new ecs.FargateService(this, "frontend2Service", {
-      cluster,
-      taskDefinition: frontend2TaskDefinition,
+    const frontendTask2: IQSTask = new QSTaskMain(this, "frontend2", {
+      stackName: this.stackName,
+      taskName: "frontend2",
+      cluster: clusterConstruct.cluster,
+      memoryLimitMiB: 512,
+      cpu: 256,
+      repo: privateEcrRepo,
+      repoTag: "latest",
+      mappedPort: 80,
       desiredCount: 1,
-      securityGroups: [ecsSecurityGroup],
-      cloudMapOptions: {
-        name: "frontend2api",
-        cloudMapNamespace: cloudmapNamespace,
-      },
+      securityGroup:
+        clusterNetworkStack.network.preconfiguredVpcCidrAccessHttpSecurityGroup,
+      executionRole: clusterConstruct.taskExecutionRole, // Set execution role for ECR pull
+      serviceDiscoveryNamespace: springbootAppNamespace,
+      DB_URL: "db@serviceIP:onPort",
+      secretsmanagerkey: "secretsmanagerkey_value",
+      EXTERNAL_GET_URL1: `http://backendapi.sbApp-springBootAppSharedPrivateNamespace/backend/api/external-api`,
+      EXTERNAL_GET_URL2: `http://backendapi.sbApp-springBootAppSharedPrivateNamespace/backend/api/greet`,
     });
 
     // Create an Application Load Balancer (ALB)
@@ -215,10 +128,10 @@ export class EcsCdkSimpleApiNlbAlbEcsDemoStack extends cdk.Stack {
       }
     );
     const appDefaultTragetGroup = applicationListener.addTargets(
-      "backend ListenerTarget",
+      "backendListenerTarget",
       {
         port: 80,
-        targets: [backendService],
+        targets: [backendTask.service],
         healthCheck: {
           interval: cdk.Duration.seconds(15),
           path: "/" + "backend" + "/actuator/health",
@@ -230,7 +143,7 @@ export class EcsCdkSimpleApiNlbAlbEcsDemoStack extends cdk.Stack {
       "frontend1ListenerTarget",
       {
         port: 80,
-        targets: [frontend1AppService],
+        targets: [frontendTask1.service],
         conditions: [
           elbv2.ListenerCondition.pathPatterns(["/" + "frontend1" + "*"]),
         ],
@@ -246,7 +159,7 @@ export class EcsCdkSimpleApiNlbAlbEcsDemoStack extends cdk.Stack {
       "frontend2ListenerTarget",
       {
         port: 80,
-        targets: [frontend2AppService],
+        targets: [frontendTask2.service],
         conditions: [
           elbv2.ListenerCondition.pathPatterns(["/" + "frontend2" + "*"]),
         ],
